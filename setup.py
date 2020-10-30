e3572d9df110032c059ccd5cc85ac968f55f2283
Viney Sunu <vineypsunu@gmail.com>
2020-10-30 15:05:20 +0300

    Update and rename setup1.py to setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="VineyS", # Replace with your own username
+    name="discord-logger-rewrite", # Replace with your own username
     version="0.0.1",
     author="VineyS",
     author_email="vineypsunu@gmail.com",
     description="A Rewritten Edition of discord_logger",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/projects/discord-logger-rewrite",
+    url="https://github.com/VineyS/python_discord_logger_rewrite",
     packages=setuptools.find_packages(),
+    install_requires=["discord-webhook == 0.8.0", "pyyaml == 5.3.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
+    python_requires='>=3',
 )
